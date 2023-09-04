e987cafc523915cd9153963d982643f4a43c79d2
Debora Peña <debo4978@gmail.com>
2023-09-04 01:50:02 -0300

    primer commit estilo y estructura

---
+++ Calculadora.js
@@ -0,0 +1,20 @@
+// una forma de darle funcionalidad es con clases:
+
+class Calculadora {
+
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+}
+
+// investigar y desarrollar formas de darle funcionalidad a la calculadora 
---
+++ index.js
@@ -0,0 +1,13 @@
+
+//accedimos con DOM
+const pantalla = document.getElementById("pantalla");
+
+
+const valorActual = document.getElementById("valor-actual");
+const valorAnterior = document.getElementById("valor-anterior");
+const operadores = document.getElementsByClassName("operador");
+const numbers = document.getElementsByClassName("number");
+
+
+
+
